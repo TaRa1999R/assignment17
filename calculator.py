333f2c22abfa2f12daccf0d10b1803d4ef4a0514
TaRa1999R <rezaietara78@gmail.com>
2023-09-13 18:01:11 +0330

    assignment17

--- calculator.py
+++ calculator.py
@@ -53,6 +53,16 @@ def nine () :
     number = number + "9"
     window.textbox.setText (number)
 
+def point () :
+    global number
+    number = number + "."
+    window.textbox.setText (number)
+
+def minus () :
+    global number
+    number = "-" + number
+    window.textbox.setText (number)
+
 def ac () :
     global number 
     number = ""
@@ -62,7 +72,7 @@ def sum () :
     global number
     global operation
     global first
-    first = int ( number )
+    first = float ( number )
     operation = "+"
     number = ""
     window.textbox.setText (number)
@@ -71,7 +81,7 @@ def sub () :
     global number
     global operation
     global first
-    first = int (number)
+    first = float (number)
     operation = "-"
     number = ""
     window.textbox.setText (number)
@@ -80,7 +90,7 @@ def multi () :
     global number 
     global operation
     global first
-    first = int (number)
+    first = float (number)
     operation = "*"
     number = ""
     window.textbox.setText (number)
@@ -89,34 +99,122 @@ def divid () :
     global number
     global operation
     global first
-    first = int (number)
+    first = float (number)
     operation = "/"
     number = ""
     window.textbox.setText (number)
 
-def sqrt () : ...
+def sqrt () : 
+    global number
+    global operation
+    global first
+    first = float (number)
+    operation = "sqrt"
+    text = f"sqrt ({first})"
+    window.textbox.setText (text)
+
+def percent () :
+    global number
+    first = float (number)
+    result = first / 100
+    window.textbox.setText (str(result))
+    number = ""
+
+def log () : 
+    global number
+    global operation
+    global first
+    first = float (number)
+    operation = "log"
+    text = f"log({number})"
+    window.textbox.setText (text)
+
+def sin () :
+    global number
+    global operation
+    global first
+    first = float (number)
+    operation = "sin"
+    text = f"sin({number})"
+    window.textbox.setText (text)
+
+def cos () : 
+    global number
+    global operation
+    global first
+    first = float (number)
+    operation = "cos"
+    text = f"cos({number})"
+    window.textbox.setText (text)
+
+def tan () : 
+    global number
+    global operation
+    global first
+    first = float (number)
+    operation = "tan"
+    text = f"tan({number})"
+    window.textbox.setText (text)
+
+def cot () : 
+    global number
+    global operation
+    global first
+    first = float (number)
+    operation = "cot"
+    text = f"cot({number})"
+    window.textbox.setText (text)
 
 def equal () :
     global number
     global first
     global operation
-    second = int ( number )
 
-    if operation == "+" :
-        result = first + second
+    if operation == "+" or operation == "-" or operation == "*" or operation == "/" :
+        second = float ( number )
 
-    elif operation == "-" :
-        result = first - second
+        if operation == "+" :
+            result = first + second
+
+        elif operation == "-" :
+            result = first - second
+        
+        elif operation == "*" :
+            result = first * second
+
+        elif operation == "/" :
+            if second == 0 :
+                result = " Error!! "
+
+            else :
+                result = first / second
     
-    elif operation == "*" :
-        result = first * second
+    else :
+        if operation == "sqrt" :
+            if first >= 0 :
+                result = math.sqrt (first)
+            
+            else :
+                result = " Error!! "
 
-    elif operation == "/" :
-        if second == 0 :
-            result = " Error!! "
+        elif operation == "log" :
+            result = math.log10 (first)
 
-        else :
-            result = first / second
+        elif operation == "sin" : 
+            rad = first * ( math.pi / 180 )
+            result = math.sin (rad)      
+
+        elif operation == "cos" :
+            rad = first * ( math.pi / 180 )
+            result = math.cos (rad)
+
+        elif operation == "tan" : 
+            rad = first * ( math.pi / 180 )
+            result = math.tan (rad)
+
+        elif operation == "cot" : 
+            rad = first * ( math.pi / 180 )
+            result = 1 / ( math.tan (rad) )
     
     window.textbox.setText (str(result))
     number = ""
@@ -140,6 +238,8 @@ window.six.clicked.connect (six)
 window.seven.clicked.connect (seven)
 window.eight.clicked.connect (eight)
 window.nine.clicked.connect (nine)
+window.point.clicked.connect (point)
+window.minus.clicked.connect (minus)
 
 window.equal.clicked.connect (equal)
 window.sum.clicked.connect (sum)
@@ -147,6 +247,12 @@ window.sub.clicked.connect (sub)
 window.multi.clicked.connect (multi)
 window.divide.clicked.connect (divid)
 window.sqrt.clicked.connect (sqrt)
+window.percent.clicked.connect (percent)
+window.log.clicked.connect (log)
+window.sin.clicked.connect (sin)
+window.cos.clicked.connect (cos)
+window.tan.clicked.connect (tan)
+window.cot.clicked.connect (cot)
 
 window.ac.clicked.connect (ac)
 
